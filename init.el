dbc1e194c23b04f8aeff0e7bb51f5b5c759f987e
Misha Zharov <mishazharov1@gmail.com>
2023-02-02 00:39:37 -0800

    Just use flymake as a default for now
    After seeing the flymake vs flycheck flamewars I think I'll stay put
    for now. In the future though it might make sense to switch to
    `lsp-mode` since it seems to support flycheck

--- init.el
+++ init.el
@@ -36,9 +36,9 @@
 (use-package straight
   :custom (straight-use-package-by-default t))
 
-(use-package flycheck
-  :ensure t
-  :config (global-flycheck-mode))
+;; (use-package flycheck
+;;  :ensure t
+;;  :config (global-flycheck-mode))
 
 (use-package vertico
   ;; Special recipe to load extensions conveniently
@@ -250,19 +250,17 @@
 			   :host github
 			   :branch "cargo-outdated-workspace"
 			   :repo "mishazharov/rustic"))
-  :config
-  (setq
-   ;; eglot seems to be the best option right now.
+  :config (setq
    rustic-lsp-client 'eglot
    rustic-format-on-save nil
-   rustic-analyzer-command '("~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/rust-analyzer")
-   ;; Prevent automatic syntax checking, which was causing lags and stutters.
-   ;; eglot-send-changes-idle-time (* 60 60)
-   )
-  ;; (add-hook 'eglot--managed-mode-hook (lambda () (flymake-mode -1)))
-  ;; Disable the annoying doc popups in the minibuffer.
-  ;; (add-hook 'eglot-managed-mode-hook (lambda () (eldoc-mode -1)))
-  )
+   rustic-analyzer-command '("~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/rust-analyzer")))
+
+;; Keeping this around *just in case eglot turns evil*
+;;
+;; (use-package eglot
+;;   :straight nil
+;;   :config (add-to-list 'eglot-stay-out-of 'flymake 'eldoc)
+;; (setq eglot-send-changes-idle-time (* 60 60)))
 
 ; From https://www.nathanfurnal.xyz/posts/building-tree-sitter-langs-emacs/
 (use-package treesit
@@ -270,36 +268,36 @@
   :commands (treesit-install-language-grammar nf/treesit-install-all-languages)
   :init
   (setq treesit-language-source-alist
-   '((bash . ("https://github.com/tree-sitter/tree-sitter-bash"))
-     (c . ("https://github.com/tree-sitter/tree-sitter-c"))
-     (cpp . ("https://github.com/tree-sitter/tree-sitter-cpp"))
-     (css . ("https://github.com/tree-sitter/tree-sitter-css"))
-     (go . ("https://github.com/tree-sitter/tree-sitter-go"))
-     (html . ("https://github.com/tree-sitter/tree-sitter-html"))
-     (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript"))
-     (json . ("https://github.com/tree-sitter/tree-sitter-json"))
-     (lua . ("https://github.com/Azganoth/tree-sitter-lua"))
-     (make . ("https://github.com/alemuller/tree-sitter-make"))
-     (ocaml . ("https://github.com/tree-sitter/tree-sitter-ocaml" "master" "ocaml/src"))
-     (python . ("https://github.com/tree-sitter/tree-sitter-python"))
-     (php . ("https://github.com/tree-sitter/tree-sitter-php"))
-     (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src"))
-     (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src"))
-     (ruby . ("https://github.com/tree-sitter/tree-sitter-ruby"))
-     (rust . ("https://github.com/tree-sitter/tree-sitter-rust"))
-     (sql . ("https://github.com/m-novikov/tree-sitter-sql"))
-     (toml . ("https://github.com/tree-sitter/tree-sitter-toml"))
-     (yaml . ("https://github.com/ikatyang/tree-sitter-yaml"))
-     (zig . ("https://github.com/GrayJack/tree-sitter-zig"))))
+	'((bash . ("https://github.com/tree-sitter/tree-sitter-bash"))
+	  (c . ("https://github.com/tree-sitter/tree-sitter-c"))
+	  (cpp . ("https://github.com/tree-sitter/tree-sitter-cpp"))
+	  (css . ("https://github.com/tree-sitter/tree-sitter-css"))
+	  (go . ("https://github.com/tree-sitter/tree-sitter-go"))
+	  (html . ("https://github.com/tree-sitter/tree-sitter-html"))
+	  (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript"))
+	  (json . ("https://github.com/tree-sitter/tree-sitter-json"))
+	  (lua . ("https://github.com/Azganoth/tree-sitter-lua"))
+	  (make . ("https://github.com/alemuller/tree-sitter-make"))
+	  (ocaml . ("https://github.com/tree-sitter/tree-sitter-ocaml" "master" "ocaml/src"))
+	  (python . ("https://github.com/tree-sitter/tree-sitter-python"))
+	  (php . ("https://github.com/tree-sitter/tree-sitter-php"))
+	  (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src"))
+	  (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src"))
+	  (ruby . ("https://github.com/tree-sitter/tree-sitter-ruby"))
+	  (rust . ("https://github.com/tree-sitter/tree-sitter-rust"))
+	  (sql . ("https://github.com/m-novikov/tree-sitter-sql"))
+	  (toml . ("https://github.com/tree-sitter/tree-sitter-toml"))
+	  (yaml . ("https://github.com/ikatyang/tree-sitter-yaml"))
+	  (zig . ("https://github.com/GrayJack/tree-sitter-zig"))))
   :config
   (defun nf/treesit-install-all-languages ()
     "Install all languages specified by `treesit-language-source-alist'."
     (interactive)
     (let ((languages (mapcar 'car treesit-language-source-alist)))
       (dolist (lang languages)
-	      (treesit-install-language-grammar lang)
-	      (message "`%s' parser was installed." lang)
-	      (sit-for 0.75)))))
+	(treesit-install-language-grammar lang)
+	(message "`%s' parser was installed." lang)
+	(sit-for 0.75)))))
 
 (setq debug-on-error t)
 (windmove-default-keybindings)
