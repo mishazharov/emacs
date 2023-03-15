e1347a446746cdf0195e657d7940b2ec60ac291d
Snake Snake <94087198+snakethatlovesstaticlibs@users.noreply.github.com>
2023-03-15 16:21:30 -0700

    Also allow consult to find hidden files

--- emacs-custom.el
+++ emacs-custom.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
+ '(consult-find-args "find . -not ( -wholename */.*/ -prune )")
  '(custom-enabled-themes '(wombat))
  '(ns-alternate-modifier 'super)
  '(ns-command-modifier 'meta))
