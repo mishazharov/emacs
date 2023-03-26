a9e8b0b73d70e53182bc141b8b16e58489ad4b23
Misha Zharov <mishazharov1@gmail.com>
2023-03-25 20:09:04 -0700

    Undo cosult find custom config because it no longer ignores dot dirs

--- emacs-custom.el
+++ emacs-custom.el
@@ -4,7 +4,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
- '(consult-find-args "find . -not ( -wholename */.*/ -prune )")
  '(custom-enabled-themes '(wombat))
  '(ns-alternate-modifier 'super)
  '(ns-command-modifier 'meta))
